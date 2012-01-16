8920729e34158681ea0226b79360f51eec298015
Kevin Davis <kgdavis@gmail.com>
2012-01-16 01:34:37 -0800

    initial commit

---
+++ presets.js
@@ -0,0 +1,10 @@
+var presets = new Object();
+
+presets.few = [200, 100, 150];
+presets.lots = [100, 200, 100, 200, 100, 150, 120, 150];
+presets.onehuge = [675, 200, 140];
+// pile on your own test cases here! these are arrays of link sizes (in px)
+
+// KNOWN ISSUES: 
+// links beyond the size of the link selector
+// at extreme numbers of links it could be slow.. 
